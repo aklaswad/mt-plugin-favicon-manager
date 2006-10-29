3b9f56d7001bf3c6222868d88701109f27091952
root <root@7739eb71-4918-0410-b9ab-8c22a6e512e9>
2006-10-29 17:39:10 +0000

    made a plugin to add favicon to cms.
    git-svn-id: file:///Users/aklaswad/repos/mtplugins/CMSFavicon@29 7739eb71-4918-0410-b9ab-8c22a6e512e9

---
+++ lib/CMSFavicon/L10N.pm
@@ -0,0 +1,7 @@
+package CMSFavicon::L10N;
+
+use strict;
+use base 'MT::Plugin::L10N';
+
+1;
+ 
---
+++ lib/CMSFavicon/L10N/ja.pm
@@ -0,0 +1,18 @@
+
+package CMSFavicon::L10N::ja;
+
+use strict;
+use base 'CMSFavicon::L10N::en_us';
+use vars qw( %Lexicon );
+
+## The following is the translation table.
+
+%Lexicon = (
+  '_PLUGIN_NAME' => 'CMSFavicon',
+  '_PLUGIN_DESCRIPTION' => '管理画面にfaviconを追加します。アイコンは自分で用意してくださいです。',
+  '_PLUGIN_AUTHOR' => '澤田 哲',
+  'favicon URL' => 'faviconのURL',
+  'enter URL address of your shortcut icon.' => 'ショートカットアイコンのURLアドレスを指定してください。',
+);
+
+1;
