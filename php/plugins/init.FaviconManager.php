6bbef496b4f8020b94810f3018fb9551afa9addd
akira <akira@7739eb71-4918-0410-b9ab-8c22a6e512e9>
2007-07-07 15:10:28 +0000

    now we can use asset_id as url.
    git-svn-id: file:///Users/aklaswad/repos/mtplugins/FaviconManager@135 7739eb71-4918-0410-b9ab-8c22a6e512e9

--- php/plugins/init.FaviconManager.php
+++ php/plugins/init.FaviconManager.php
@@ -1,15 +1,36 @@
 <?php
 
 global $mt;
-$ctx = &$mt->context();
+$smarty =& $mt->context();
+$smarty->register_outputfilter("faviconmanager_add_favicon");
 
 function faviconmanager_add_favicon($tmpl, &$ctx){
-    $insert_text = '<link rel="shortcut icon" href="http://www.msn.com/favicon.ico" />' . "\n    <link";
-    $tmpl = preg_replace('!<link!', $insert_text, $tmpl, 1);
+    $blogid = $ctx->stash('blog_id');
+    $scope = 'blog:' . $blogid;
+    $config = $ctx->mt->db->fetch_plugin_config('Favicon Manager', $scope);
+
+    if ($config) {
+        $setting_str = $config['faviconmanager_blog_icon'];
+        $favicon_url = '';
+
+        if (preg_match('/^asset:/', $setting_str)) {
+            $asset_id = preg_replace('/^asset:/', '', $setting_str, 1);
+            $args['asset_id'] = $asset_id;
+            $asset = $ctx->mt->db->fetch_assets($args);
+            if ($asset) {
+                $favicon_url = $asset[0]['asset_url'];
+            }
+        }
+        else {
+            $favicon_url = $setting_str;
+        }
+
+        if ($favicon_url) {
+            $insert_text = '<link rel="shortcut icon" href="' . $favicon_url . '" />' . "\n   <link";
+            $tmpl = preg_replace('/<link/', $insert_text, $tmpl, 1);
+        }
+    }
     return $tmpl;
-
 }
-
-$ctx->register_outputfilter("faviconmanager_add_favicon");
 
 ?>
--- plugins/FaviconManager/faviconmanager.pl
+++ plugins/FaviconManager/faviconmanager.pl
@@ -8,41 +8,51 @@ $VERSION = '0.2';
 @MT::Plugin::FaviconManager::ISA = qw(MT::Plugin);
 
 my $plugin = new MT::Plugin::FaviconManager({
-  name => '<MT_TRANS phrase=\'_PLUGIN_NAME\'>',
-  version => $VERSION,
-  description => '<MT_TRANS phrase=\'_PLUGIN_DESCRIPTION\'>',
-  author_name => '<MT_TRANS phrase=\'_PLUGIN_AUTHOR\'>',
-  author_link => 'http://blog.aklaswad.com/',
-  settings => new MT::PluginSettings([
-    ['faviconmanager_cms_icon', { Scope => 'system'}],
-    ['faviconmanager_blog_icon'],
-  ]),
-  system_config_template => 'config.tmpl',
-  blog_config_template => 'blogconfig.tmpl',
-  doc_link => 'http://blog.aklaswad.com/',
-  l10n_class => 'FaviconManager::L10N',
+    name => 'Favicon Manager',
+    version => $VERSION,
+    description => '<MT_TRANS phrase=\'_PLUGIN_DESCRIPTION\'>',
+    author_name => '<MT_TRANS phrase=\'_PLUGIN_AUTHOR\'>',
+    author_link => 'http://blog.aklaswad.com/',
+    settings => new MT::PluginSettings([
+      ['faviconmanager_cms_icon', { Scope => 'system'}],
+      ['faviconmanager_blog_icon'],
+    ]),
+    system_config_template => 'config.tmpl',
+    blog_config_template => 'blogconfig.tmpl',
+    doc_link => 'http://blog.aklaswad.com/',
+    l10n_class => 'FaviconManager::L10N',
 });
 
 MT->add_plugin($plugin);
 MT->add_callback('MT::App::CMS::template_source.header', 9, $plugin, \&_add_cmsfavicon);
-MT->add_callback('build_page', 9, $plugin, \&_add_favicon);
+MT->add_callback('build_page', 9, $plugin, \&_add_blog_favicon);
 
 sub get_favicon_url {
     my $plugin = shift;
     my $blog_id = shift;
-    my $url = '';
+    my ($setting_str, $favicon_url);
+
     if ($blog_id) {
         my $config =  $plugin->get_config_hash('blog:' . $blog_id);
-        $url = $config->{faviconmanager_blog_icon};
+        $setting_str = $config->{faviconmanager_blog_icon};
     }
-    unless ($url) {
+    else {
         my $config =  $plugin->get_config_hash('system');
-        $url = $config->{faviconmanager_cms_icon};
+        $setting_str = $config->{faviconmanager_cms_icon};
     }
 
-#    my $nonurlchar = '[^A-Za-z0-9\-\_\.\/\~\,\$\!\*\'\(\)\;\:\@\=\&\+]';
-#    die "out of URL characters in Favicon URL" if $url =~ /$nonurlchar/;
-    return $url;
+    if ($setting_str =~ /^asset:/) {
+        my $asset_id = $setting_str =~ s/asset://;
+        require MT::Asset;
+        my $asset = MT::Asset->load({id => $asset_id});
+        die "Favicon Manager failed: can't load asset object." unless $asset;
+        $favicon_url = $asset->url;
+    }
+    else {
+        $favicon_url = $setting_str;
+    }
+
+    return $favicon_url;
 }
 
 sub _add_cmsfavicon {
@@ -56,21 +66,21 @@ EOT
     $$tmpl_ref = $add . $$tmpl_ref;
 }
 
-sub _add_favicon {
+sub _add_blog_favicon {
     my ($eh, %param) = @_;
-    my $tmpl = $param{'Template'};
-    die "no tmpl" unless defined $tmpl;
-    my $blog = $param{'Blog'};
-    my $url = $plugin->get_favicon_url($blog->id);
-    die "no url" unless $url;
-    my $add = "<link rel=\"shortcut icon\" href=\"$url\" />\n    <link";
-    my $file = $param{'File'};
-    if ($file =~ /html$/) {
-        my $html = $param{'Content'};
-        $$html =~ s{<link}{$add};
-    }
-}
 
+    my $file = $param{'File'};
+    return if $file !~ /html$/;
+
+    my $tmpl = $param{'Template'}
+        or return;
+    my $blog = $param{'Blog'};
+    my $favicon_url = $plugin->get_favicon_url($blog->id)
+        or return;
+    my $add = "<link rel=\"shortcut icon\" href=\"$favicon_url\" />\n   <link";
+    my $html = $param{'Content'};
+    $$html =~ s{<link}{$add};
+}
 
 1;
 
